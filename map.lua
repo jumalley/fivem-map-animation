74e032f5b0394b3ad0569ac8c060a69fec00fc4a
Ju <jumalleydev@gmail.com>
2025-04-25 08:31:16 +0200

    Update map.lua

--- map.lua
+++ map.lua
@@ -1,76 +1,72 @@
 local inMenuMode = false
-local Animation = false
-Citizen.CreateThread(function()
-	while true do
-		Wait(500)
-		if IsPauseMenuActive() then
+local animationPlaying = false
+local playerProps = {}
 
-			AnimMode()
-		else
-			if Animation then
-				Animation = false
-				ClearPedTasks(PlayerPedId())
-			end
-			inMenuMode = false
-			DestroyAllProps()
-		end
-	end
+local animDict = "amb@world_human_tourist_map@male@idle_b"
+local animName = "idle_d"
+local propModel = "p_tourist_map_01_s"
+local propBone = 28422
 
+CreateThread(function()
+    while true do
+        Wait(500)
+
+        local player = PlayerPedId()
+
+        if IsPauseMenuActive() and not inMenuMode and not IsPedInAnyVehicle(player) then
+            playMapAnimation(player)
+        elseif not IsPauseMenuActive() then
+            if animationPlaying then
+                ClearPedTasks(player)
+                animationPlaying = false
+            end
+            inMenuMode = false
+            destroyAllProps()
+        end
+    end
 end)
 
-function AnimMode()
-	if not inMenuMode then
-		if not IsPedInAnyVehicle(PlayerPedId()) then
-			Animation = true
-			inMenuMode = true
-			local Player = PlayerPedId()
-			local AnimDict = "amb@world_human_tourist_map@male@idle_b"
-			local Anim = "idle_d"
-			LoadAnim(AnimDict)
-			local Prop = 'p_tourist_map_01_s'
-			local PropBone = 28422
-			AddPropToPlayer(Prop, PropBone, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-			TaskPlayAnim(GetPlayerPed(-1), AnimDict, Anim, 2.0, 8.0, -1, 53, 0, false, false, false)
-		end
-	else
+function playMapAnimation(player)
+    animationPlaying = true
+    inMenuMode = true
 
-	end
+    loadAnim(animDict)
+    addPropToPlayer(propModel, propBone, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+
+    TaskPlayAnim(player, animDict, animName, 2.0, 8.0, -1, 53, 0, false, false, false)
 end
 
-function LoadAnim(dict)
-  while not HasAnimDictLoaded(dict) do
-    RequestAnimDict(dict)
-    Wait(10)
-  end
+function loadAnim(dict)
+    while not HasAnimDictLoaded(dict) do
+        RequestAnimDict(dict)
+        Wait(10)
+    end
 end
 
-function LoadPropDict(model)
-  while not HasModelLoaded(GetHashKey(model)) do
-    RequestModel(GetHashKey(model))
-    Wait(10)
-  end
+function loadModel(model)
+    local modelHash = GetHashKey(model)
+    while not HasModelLoaded(modelHash) do
+        RequestModel(modelHash)
+        Wait(10)
+    end
 end
 
-local PlayerHasProp = false
-local PlayerProps = {}
-function AddPropToPlayer(prop1, bone, off1, off2, off3, rot1, rot2, rot3)
-  local Player = PlayerPedId()
-  local x,y,z = table.unpack(GetEntityCoords(Player))
+function addPropToPlayer(model, bone, offX, offY, offZ, rotX, rotY, rotZ)
+    local player = PlayerPedId()
+    local x, y, z = table.unpack(GetEntityCoords(player))
 
-  if not HasModelLoaded(prop1) then
-    LoadPropDict(prop1)
-  end
+    loadModel(model)
 
-  prop = CreateObject(GetHashKey(prop1), x, y, z+0.2,  true,  true, true)
-  AttachEntityToEntity(prop, Player, GetPedBoneIndex(Player, bone), off1, off2, off3, rot1, rot2, rot3, true, true, false, true, 1, true)
-  table.insert(PlayerProps, prop)
-  PlayerHasProp = true
-  SetModelAsNoLongerNeeded(prop1)
+    local prop = CreateObject(GetHashKey(model), x, y, z + 0.2, true, true, true)
+    AttachEntityToEntity(prop, player, GetPedBoneIndex(player, bone), offX, offY, offZ, rotX, rotY, rotZ, true, true, false, true, 1, true)
+    table.insert(playerProps, prop)
+
+    SetModelAsNoLongerNeeded(model)
 end
 
-function DestroyAllProps()
-  for _,v in pairs(PlayerProps) do
-    DeleteEntity(v)
-  end
-  PlayerHasProp = false
+function destroyAllProps()
+    for _, prop in pairs(playerProps) do
+        DeleteEntity(prop)
+    end
+    playerProps = {}
 end
